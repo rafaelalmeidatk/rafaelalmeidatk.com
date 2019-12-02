55bd73ac8ba8388233d556aa3d994ed0c6de2f9b
Rafael Almeida <rafaelalmeidatk@gmail.com>
2019-12-02 07:54:34 -0300

    Add more content to the header, add noise background

---
+++ src/components/Noise.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+const Noise = () => (
+  <div className="noise">
+    <style jsx>{`
+      .noise {
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        background-image: url("/imgs/noise.png");
+        animation: 0.2s infinite noise;
+      }
+
+      @keyframes noise {
+        0%,
+        100% {
+          background-position: 0 0;
+        }
+        20% {
+          background-position: -50% 50%;
+        }
+        40% {
+          background-position: 10% 17%;
+        }
+        60% {
+          background-position: 73% 7%;
+        }
+        80% {
+          background-position: 45% 65%;
+        }
+      }
+    `}</style>
+  </div>
+);
+
+export default Noise;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,32 @@
 import React from "react";
 import Head from "next/head";
+import Noise from "../components/Noise";
+
+// This file will be a mess until I figure out what I want to do :P
+
+const ProjectCard = ({ title }) => {
+  return (
+    <div className="container">
+      <div className="image" />
+
+      <div className="title">Twitter Fullstack Clone</div>
+
+      <style jsx>{`
+        .image {
+          border: 1px solid #1f1f1f;
+          width: 100%;
+          height: 94px;
+        }
+
+        .title {
+          margin-top: 12px;
+          font-size: 1.4em;
+          font-weight: 6 00;
+        }
+      `}</style>
+    </div>
+  );
+};
 
 const Home = () => (
   <div>
@@ -8,8 +35,16 @@ const Home = () => (
     </Head>
 
     <section className="heading">
+      <Noise />
       <div className="container">
-        <h1>Hi</h1>
+        <h1>
+          Hi! I'm <span className="highlight">Rafael Almeida</span>, and I try
+          to not break the web
+        </h1>
+        <p>
+          I am mostly working with bleeding edge technologies, learning
+          something new almost everyday.
+        </p>
       </div>
     </section>
 
@@ -18,20 +53,35 @@ const Home = () => (
         <h2>My projects</h2>
         <p>Lorem ipsum dolor sit amet</p>
 
-        <div className="columns"></div>
+        <div className="columns">
+          <div className="column">
+            <ProjectCard />
+          </div>
+          <div className="column">Second column</div>
+        </div>
       </div>
     </section>
 
     <style jsx>{`
       .heading {
+        position: relative;
         padding: 10rem 2rem;
         color: #fff;
-        background: #1f1f1f;
+        background: #121212;
       }
 
-      h1 {
+      .heading h1 {
         font-size: 4.5em;
         font-weight: 740;
+        line-height: 1em;
+        letter-spacing: -0.025em;
+      }
+
+      .heading p {
+        margin-top: 16px;
+        font-size: 1.5em;
+        font-weight: 300;
+        letter-spacing: -0.06em;
       }
 
       .projects {
