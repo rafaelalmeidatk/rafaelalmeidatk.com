f9b35bdfc2bd803c67dacc21aa1e7429e800344a
Rafael Almeida <rafaelalmeidatk@gmail.com>
2020-02-04 15:22:22 -0300

    Add flash animation on header

--- src/pages/index.js
+++ src/pages/index.js
@@ -37,22 +37,24 @@ const Home = () => (
     <section className="heading">
       <Noise />
       <div className="container">
-        <h1>
-          Hi! I am <span className="highlight">Rafael Almeida</span>, and I try
-          to not break the web
-        </h1>
+        <div className="flash-animation">
+          <h1>
+            Hi! I am <span className="highlight">Rafael Almeida</span>, and I
+            try to not break the web
+          </h1>
 
-        <p>
-          I am mostly working with bleeding edge technologies,{" "}
-          <a
-            href="https://github.com/rafaelalmeidatk/TIL"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            learning something new almost everyday
-          </a>
-          .
-        </p>
+          <p>
+            I am mostly working with bleeding edge technologies,{" "}
+            <a
+              href="https://github.com/rafaelalmeidatk/TIL"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              learning something new almost everyday
+            </a>
+            .
+          </p>
+        </div>
       </div>
     </section>
 
@@ -155,6 +157,74 @@ const Home = () => (
         font-size: 1.5em;
         font-weight: 300;
         letter-spacing: -0.06em;
+      }
+
+      .flash-animation {
+        animation: heading-flash 0.7s steps(1, end);
+      }
+
+      @keyframes heading-flash {
+        0% {
+          clip-path: inset(0 70% 0 0);
+          background: #fff;
+          opacity: 100%;
+        }
+
+        10% {
+          opacity: 0%;
+        }
+
+        15% {
+          opacity: 100%;
+        }
+
+        20% {
+          opacity: 0%;
+        }
+
+        25% {
+          opacity: 100%;
+        }
+
+        30% {
+          opacity: 0%;
+        }
+
+        45% {
+          opacity: 100%;
+        }
+
+        50% {
+          clip-path: inset(0 0% 0 0);
+        }
+
+        55% {
+          clip-path: inset(0 40% 0 0);
+        }
+
+        65% {
+          opacity: 0%;
+        }
+
+        70% {
+          opacity: 100%;
+        }
+
+        75% {
+          clip-path: inset(0 0% 0 0);
+        }
+
+        80% {
+          opacity: 0%;
+        }
+
+        85% {
+          opacity: 100%;
+        }
+
+        100% {
+          background: #121212;
+        }
       }
 
       .about-me .box {
