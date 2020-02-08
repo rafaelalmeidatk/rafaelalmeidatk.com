3621df889a7095116984bc8ab3de37e1662b7570
Rafael Almeida <rafaelalmeidatk@gmail.com>
2020-02-07 22:57:25 -0300

    add show animation to project cards

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const ProjectCard = ({ title, cover }) => (
-  <div className="container">
-    <div className="cover" />
-    <div className="square" />
-    <div className="title">{title}</div>
+const ProjectCard = ({ title, cover }) => {
+  const [animationNumber, setAnimationNumber] = useState(1);
 
-    <style jsx>{`
+  useEffect(() => {
+    setAnimationNumber(Math.floor(Math.random() * 3) + 1);
+  });
+
+  return (
+    <div className="container">
+      <div className="cover" style={{ backgroundImage: `url(${cover})` }} />
+      <div className="square" />
+      <div className={`title top top-anim-${animationNumber}`}>{title}</div>
+      <div className={`title bottom bottom-anim-${animationNumber}`}>
+        {title}
+      </div>
+
+      <style jsx>{`
         .container {
           position: relative;
           padding: 40px 20px;
@@ -17,6 +27,17 @@ const ProjectCard = ({ title, cover }) => (
           justify-content: center;
           align-items: center;
           overflow: hidden;
+
+          opacity: 0%;
+          animation: glitch 0.2s steps(1, end) 1.4s forwards;
+        }
+
+        .container:hover .cover {
+          opacity: 0.7;
+        }
+
+        .container:hover .title {
+          opacity: 0.8;
         }
 
         .cover {
@@ -26,19 +47,10 @@ const ProjectCard = ({ title, cover }) => (
           right: 0;
           bottom: 0;
           border: 1px solid #1f1f1f;
-          background-image: url("${cover}");
           background-size: cover;
           background-position: center center;
           opacity: 0;
           transition: opacity 0.2s ease;
-        }
-
-        .container:hover .cover {
-          opacity: 0.7;
-        }
-
-        .container:hover .title {
-          opacity: 0.8;
         }
 
         .square {
@@ -51,21 +63,198 @@ const ProjectCard = ({ title, cover }) => (
           transition: all 0.2s ease;
         }
 
-        .container:hover .square {
-          width: 350px;
-          height: 350px;
-          opacity: 0.7;
-        }
-
         .title {
-          position: relative;
+          position: absolute;
           text-transform: uppercase;
           font-size: 1.3em;
           letter-spacing: 0.1em;
           font-weight: 600;
+
+          opacity: 0%;
+          animation: glitch-text 0.4s steps(1, end) 1.4s forwards;
+        }
+
+        .top {
+          clip-path: inset(0% 0% 50% 0);
+        }
+
+        .top-anim-1 {
+          animation: glitch-text-top-1 0.4s steps(1, end) 1.4s forwards;
+        }
+
+        .top-anim-2 {
+          animation: glitch-text-top-2 0.4s steps(1, end) 1.4s forwards;
+        }
+
+        .top-anim-3 {
+          animation: glitch-text-top-3 0.4s steps(1, end) 1.4s forwards;
+        }
+
+        .container:hover .top {
+          transform: translateX(2px);
+        }
+
+        .bottom {
+          clip-path: inset(50% 0 0 0);
+          animation: glitch-text-bottom 0.4s steps(1, end) 1.4s forwards;
+        }
+
+        .bottom-anim-1 {
+          animation: glitch-text-bottom-1 0.4s steps(1, end) 1.4s forwards;
+        }
+
+        .bottom-anim-2 {
+          animation: glitch-text-bottom-2 0.4s steps(1, end) 1.4s forwards;
+        }
+
+        .bottom-anim-3 {
+          animation: glitch-text-bottom-3 0.4s steps(1, end) 1.4s forwards;
+        }
+
+        .container:hover .bottom {
+          transform: translateX(-2px);
+        }
+
+        @keyframes glitch {
+          0% {
+            opacity: 0%;
+          }
+
+          40% {
+            opacity: 100%;
+          }
+
+          80% {
+            opacity: 0%;
+          }
+
+          100% {
+            opacity: 100%;
+          }
+        }
+
+        @keyframes glitch-text-top-1 {
+          0% {
+            opacity: 100%;
+            transform: translateX(6px);
+          }
+
+          40% {
+            transform: translateX(-6px);
+          }
+
+          80% {
+            transform: translate(2px 3px);
+          }
+
+          100% {
+            transform: translate(0 0);
+            opacity: 100%;
+          }
+        }
+
+        @keyframes glitch-text-top-2 {
+          0% {
+            opacity: 100%;
+            transform: translateX(1px);
+          }
+
+          40% {
+            transform: translateX(-8px);
+          }
+
+          80% {
+            transform: translate(1px 6px);
+          }
+
+          100% {
+            transform: translate(0 0);
+            opacity: 100%;
+          }
+        }
+
+        @keyframes glitch-text-top-3 {
+          0% {
+            opacity: 100%;
+            transform: translateX(4px);
+          }
+
+          40% {
+            transform: translateX(-2px);
+          }
+
+          80% {
+            transform: translate(5px 1px);
+          }
+
+          100% {
+            transform: translate(0 0);
+            opacity: 100%;
+          }
+        }
+
+        @keyframes glitch-text-bottom-1 {
+          0% {
+            opacity: 100%;
+            transform: translateX(-6px);
+          }
+
+          40% {
+            transform: translateX(4px);
+          }
+
+          80% {
+            transform: translate(2px 1px);
+          }
+
+          100% {
+            transform: translate(0 0);
+            opacity: 100%;
+          }
+        }
+
+        @keyframes glitch-text-bottom-2 {
+          0% {
+            opacity: 100%;
+            transform: translateX(-4px);
+          }
+
+          40% {
+            transform: translateX(8px);
+          }
+
+          80% {
+            transform: translate(4px -3px);
+          }
+
+          100% {
+            transform: translate(0 0);
+            opacity: 100%;
+          }
+        }
+
+        @keyframes glitch-text-bottom-3 {
+          0% {
+            opacity: 100%;
+            transform: translateX(-2px);
+          }
+
+          40% {
+            transform: translateX(2px);
+          }
+
+          80% {
+            transform: translate(3px -1px);
+          }
+
+          100% {
+            transform: translate(0 0);
+            opacity: 100%;
+          }
         }
       `}</style>
-  </div>
-);
+    </div>
+  );
+};
 
 export default ProjectCard;
