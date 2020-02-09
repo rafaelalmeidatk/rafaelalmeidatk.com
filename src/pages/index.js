900add36327acd15f355dfa0cde7fdebdb341925
Rafael Almeida <rafaelalmeidatk@gmail.com>
2020-02-09 00:12:35 -0300

    delete noise component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,10 @@
 import React from "react";
-import Head from "next/head";
-import Noise from "../components/Noise";
 import Heading from "../components/Heading";
 import AboutMe from "../components/AboutMe";
 import Projects from "../components/Projects";
 
 const Home = () => (
   <>
-    <Noise />
-
     <Heading />
     <AboutMe />
     <Projects />
