8a8118bc56ce078ec437452d2bb9ba90e56135d5
Rafael Almeida <rafaelalmeidatk@gmail.com>
2020-06-15 17:52:56 -0300

    Add a github link in the blog posts page to remove this blank space

--- src/components/blog/Header.tsx
+++ src/components/blog/Header.tsx
@@ -10,9 +10,18 @@ const Header = ({ isBlogPost = false }: HeaderProps) => (
     <a href="/" className={styles.name}>
       rafael almeida
     </a>
-    {isBlogPost && (
+    {isBlogPost ? (
       <a href="/blog" className={styles.allPosts}>
         all posts
+      </a>
+    ) : (
+      <a
+        href="https://github.com/rafaelalmeidatk"
+        target="_blank"
+        rel="noopener noreferrer"
+        className={styles.allPosts}
+      >
+        my github
       </a>
     )}
   </header>
