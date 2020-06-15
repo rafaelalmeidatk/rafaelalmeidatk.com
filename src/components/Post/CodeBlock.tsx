626a125c425904225ad4a55071a16e7e9ea18d0b
Rafael Almeida <rafaelalmeidatk@gmail.com>
2020-06-15 05:45:49 -0300

    Add syntax highlight

---
+++ src/components/Post/CodeBlock.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import Highlight, { defaultProps, Language } from 'prism-react-renderer';
+import vsDark from 'prism-react-renderer/themes/vsDark';
+
+type CodeBlockProps = {
+  className: string;
+  children: string;
+};
+
+const CodeBlock = ({ children, className }: CodeBlockProps) => {
+  const language = className.replace(/language-/, '') as Language;
+  let code = children;
+  if (code[code.length - 1] === '\n') {
+    code = children.substr(0, children.length - 1); // remove the new line at the end
+  }
+
+  return (
+    <Highlight {...defaultProps} code={code} language={language} theme={vsDark}>
+      {({ className, style, tokens, getLineProps, getTokenProps }) => (
+        <pre className={className} style={{ ...style, padding: '20px' }}>
+          {tokens.map((line, i) => (
+            <div key={i} {...getLineProps({ line, key: i })}>
+              {line.map((token, key) => (
+                <span key={key} {...getTokenProps({ token, key })} />
+              ))}
+            </div>
+          ))}
+        </pre>
+      )}
+    </Highlight>
+  );
+};
+
+export default CodeBlock;
--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import Head from 'next/head';
+import { MDXProvider } from '@mdx-js/react';
 import BlogWrapper from '../Blog/BlogWrapper';
 import BlogHeader from '../Blog/BlogHeader';
 import PostHeader from './PostHeader';
+import CodeBlock from './CodeBlock';
 import styles from './Post.module.css';
+
+const components = {
+  pre: (props: any) => <div {...props} />,
+  code: CodeBlock,
+};
 
 type PostProps = {
   meta: {
@@ -25,7 +32,9 @@ const Post = ({ children, meta }: PostProps) => (
       <article>
         <PostHeader title={meta.title} time={meta.date} />
 
-        <section className={styles.post}>{children}</section>
+        <MDXProvider components={components}>
+          <section className={styles.post}>{children}</section>
+        </MDXProvider>
       </article>
     </BlogWrapper>
   </>
