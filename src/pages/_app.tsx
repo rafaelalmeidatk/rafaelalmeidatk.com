0cd9194032416e64eeaec907408a7b4e8232d4a6
Rafael Almeida <rafaelalmeidatk@gmail.com>
2022-05-13 21:30:32 -0300

    Update react to latest, add Chakra UI

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,29 +1,24 @@
 import Head from 'next/head';
 import { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
 import Seo from '../components/Seo';
 import '../lib/bulma.scss';
 import '../styles/global.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ChakraProvider>
       <Head>
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700;800&display=swap"
           rel="stylesheet"
-        />
-        <meta charSet="utf-8" />
-        <meta
-          key="viewport"
-          name="viewport"
-          content="width=device-width, initial-scale=1"
         />
       </Head>
 
       <Seo description="Software Developer, learning how to break stuff in different ways" />
 
       <Component {...pageProps} />
-    </>
+    </ChakraProvider>
   );
 }
 
