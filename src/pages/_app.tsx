a5189d2d9e4bd34c9760eb3cc161d75f204b7d9e
Rafael Almeida <rafaelalmeidatk@gmail.com>
2022-06-19 17:57:17 -0300

    Import font in custom document

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,3 @@
-import Head from 'next/head';
 import { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import Seo from '../components/Seo';
@@ -7,13 +6,6 @@ import chakraTheme from '../lib/chakraTheme';
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={chakraTheme}>
-      <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-
       <Seo description="Software Developer, learning how to break stuff in different ways" />
 
       <Component {...pageProps} />
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,12 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
