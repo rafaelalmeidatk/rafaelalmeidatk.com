1c82b06f2228f25e1ec523f24869566199db2f49
Rafael Almeida <rafaelalmeidatk@gmail.com>
2022-06-20 22:01:44 -0300

    Add theme toggle and GitHub icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,19 @@
-import { Flex, Link, chakra } from '@chakra-ui/react';
+import { MoonIcon, SunIcon } from '@chakra-ui/icons';
+import {
+  Flex,
+  Link,
+  chakra,
+  IconButton,
+  LinkBox,
+  Stack,
+  useColorMode,
+} from '@chakra-ui/react';
+import { GitHubIcon } from './GitHubIcon';
+import NextLink from 'next/link';
 
 export const Header = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+
   return (
     <Flex
       as="header"
@@ -8,14 +21,47 @@ export const Header = () => {
       alignItems="stretch"
       marginX={-3}
     >
-      <Link variant="header">/</Link>
+      <Stack direction="row" alignItems="center">
+        <NextLink href="/" passHref>
+          <Link variant="header">/</Link>
+        </NextLink>
 
-      <Flex>
-        <Link variant="header" mr={1}>
-          /blog
+        <NextLink href="/blog" passHref>
+          <Link variant="header">/blog</Link>
+        </NextLink>
+      </Stack>
+
+      <Stack alignItems="center" direction="row" spacing={3}>
+        <Link
+          aria-label="My GitHub profile"
+          href="https://github.com/rafaelalmeidatk"
+          isExternal
+          color="currentColor"
+          transition="opacity .2s ease"
+          opacity={0.7}
+          _hover={{ opacity: 0.9 }}
+        >
+          <GitHubIcon boxSize={7} />
         </Link>
-        <Link variant="header">/github</Link>
-      </Flex>
+
+        <IconButton
+          onClick={toggleColorMode}
+          aria-label={`Switch to ${
+            colorMode === 'light' ? 'dark' : 'light'
+          } mode`}
+          icon={
+            colorMode === 'light' ? (
+              <MoonIcon boxSize={7} />
+            ) : (
+              <SunIcon boxSize={7} />
+            )
+          }
+          variant="ghost"
+          transition="opacity .2s ease"
+          opacity={0.7}
+          _hover={{ opacity: 0.9 }}
+        />
+      </Stack>
     </Flex>
   );
 };
