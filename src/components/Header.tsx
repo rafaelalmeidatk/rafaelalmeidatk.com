081551a35f2602c67a6782bed3d7699570b9570b
Rafael Almeida <rafaelalmeidatk@gmail.com>
2022-09-25 03:50:11 -0300

    Add dark mode support with next-themes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,13 @@
-import { useColorMode } from '@chakra-ui/react';
+import { useTheme } from 'next-themes';
 import { GitHubIcon, MoonIcon, SunIcon } from './icons';
 import NextLink from 'next/link';
 
 const MENU_ITEMS = ['/', '/blog'];
 
 export const Header = () => {
-  const { colorMode, toggleColorMode } = useColorMode();
+  const { theme, setTheme } = useTheme();
+
+  const toggleTheme = () => setTheme(theme === 'light' ? 'dark' : 'light');
 
   return (
     <header className="flex justify-between items-center -mx-3">
@@ -31,13 +33,11 @@ export const Header = () => {
         </a>
 
         <button
-          onClick={toggleColorMode}
-          aria-label={`Switch to ${
-            colorMode === 'light' ? 'dark' : 'light'
-          } mode`}
+          onClick={toggleTheme}
+          aria-label={`Switch to ${theme === 'light' ? 'dark' : 'light'} mode`}
           className="transition w-10 opacity-70 hover:opacity-90"
         >
-          {colorMode === 'light' ? <MoonIcon size={7} /> : <SunIcon size={8} />}
+          {theme === 'light' ? <MoonIcon size={7} /> : <SunIcon size={8} />}
         </button>
       </div>
     </header>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
+import { ThemeProvider } from 'next-themes';
 import { Seo } from '../components/Seo';
 import chakraTheme from '../lib/chakraTheme';
 
@@ -7,11 +8,13 @@ import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={chakraTheme}>
-      <Seo description="Software Engineer building full-stack web applications with React, Node.js, TypeScript, and PostgreSQL" />
+    <ThemeProvider defaultTheme="dark" attribute="class">
+      <ChakraProvider theme={chakraTheme}>
+        <Seo description="Software Engineer building full-stack web applications with React, Node.js, TypeScript, and PostgreSQL" />
 
-      <Component {...pageProps} />
-    </ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </ThemeProvider>
   );
 }
 
